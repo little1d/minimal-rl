25188d23c7f688a6d517f58a4abac742eebb9f26
Zachary_Yang <2431542525@qq.com>
2024-03-20 20:34:37 +0800

    train_net func

--- actor_critic.py
+++ actor_critic.py
@@ -8,6 +8,7 @@ from torch.distributions import Categorical
 
 # Hyperparameters
 learning_rate = 0.002
+# 折扣因子，用来平衡当前即时奖励和未来长期奖励的影响，当折扣因子接近于1时，算法倾向于长期奖励；当折扣因子接近于0时，算法倾向于即时奖励。
 gamma = 0.98
 n_rollout = 10
 
@@ -51,3 +52,18 @@ class ActorCritic(nn.Module):
                                                                torch.tensor(done_list, dtype=torch.float)
         self.data = []
         return s_batch, a_batch, r_batch, s_prime_batch, done_batch
+    
+    def train_net(self):
+        s, a, r, s_prime, done = self.make_batch()
+        td_target = r + gamma * self.v(s_prime) * done
+        delta = td_target - self.v(s)
+        
+        pi = self.pi(s, softmax_dim=1)
+        pi_a = pi.gather(1, a)
+        loss = -torch.log(pi_a) * delta.detach() + F.smooth_l1_loss(self.v(s), td_target.detach())
+        
+        self.optimizer.zero_grad()
+        loss.mean().backward()
+        self.optimizer.step()
+        
+        
