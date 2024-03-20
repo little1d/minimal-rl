6d73c4f1759210811d30434cde7c6bd4aa49d4e2
Zachary_Yang <2431542525@qq.com>
2024-03-20 20:38:47 +0800

    main fucntion

--- actor_critic.py
+++ actor_critic.py
@@ -66,4 +66,40 @@ class ActorCritic(nn.Module):
         loss.mean().backward()
         self.optimizer.step()
         
+def main():
+    env = gym.make('CartPole-v1')
+    model = ActorCritic()
+    print_interval = 20
+    score = 0.0
+    
+    for n_epi in range(10000):
+        done = False
+        s, _ = env.reset()
+        while not done:
+            for t in range(n_rollout):
+                prob = model.pi(torch.from_numpy(s).float())
+                m = Categorical(prob)
+                a = m.sample().item()
+                s_prime, r, done, truncated, info = env.step(a)
+                model.put_data((s, a, r, s_prime, done))
+                
+                s = s_prime
+                score += r
+                
+                if done:
+                    break
+                
+            model.train_net()
+            
+        if n_epi % print_interval == 0 and n_epi != 0:
+            print("# of episode :{}, avg score : {:.1f}".format(n_epi, score/print_interval))
+            score = 0.0
+        env.close()
         
+    
+if __name__ == '__main__':
+    main()
+            
+
+            
+            
