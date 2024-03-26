2981e71cfa43682806454683200d969bf08de2ff
Zachary_Yang <2431542525@qq.com>
2024-03-26 15:54:59 +0800

    ppo swanlab

--- ppo.py
+++ ppo.py
@@ -5,14 +5,23 @@ import torch.optim as optim
 import torch.nn.functional as F
 from torch.distributions import Categorical
 
+import swanlab
+
 # Hyperparameters
-#Hyperparameters
 learning_rate = 0.0005
 gamma         = 0.98
 lmbda         = 0.95
 eps_clip      = 0.1
 K_epoch       = 3
 T_horizon     = 20
+
+# swanlab init
+swanlab.init(
+    experiment_name="ppo method",
+    description= "a simple rl task",
+    logdir="./swanlog",
+)
+
 
 class PPO(nn.Module):
     def __init__(self):
@@ -87,36 +96,38 @@ class PPO(nn.Module):
         loss.mean().backward()
         self.optimizer.step()
         
-    def main():
-        env = gym.make('CartPole-v1')
-        model = PPO()
+def main():
+    env = gym.make('CartPole-v1')
+    model = PPO()
+    score = 0.0
+    print_interval = 20
+    
+    for n_epi in range(10000):
+        s, _ = env.reset()
+    done = False
+    while not done:
+        for t in range(T_horizon):
+            prob = model.pi(torch.from_numpy(s).float())
+            m = Categorical(prob)
+            a = m.sample().item()
+            s_prime, r, done, truncated, info = env.step(a)
+
+            model.put_data((s, a, r/100.0, s_prime, prob[a].item(), done))
+            s = s_prime
+
+            score += r
+            if done:
+                break
+
+        model.train_net()
+
+    if n_epi%print_interval==0 and n_epi!=0:
+        print("# of episode :{}, avg score : {:.1f}".format(n_epi, score/print_interval))
+        swanlab.log({"avg_score": score/print_interval})
         score = 0.0
-        print_interval = 20
-        
-        for n_epi in range(10000):
-            s, _ = env.reset()
-        done = False
-        while not done:
-            for t in range(T_horizon):
-                prob = model.pi(torch.from_numpy(s).float())
-                m = Categorical(prob)
-                a = m.sample().item()
-                s_prime, r, done, truncated, info = env.step(a)
 
-                model.put_data((s, a, r/100.0, s_prime, prob[a].item(), done))
-                s = s_prime
+    env.close()
 
-                score += r
-                if done:
-                    break
-
-            model.train_net()
-
-        if n_epi%print_interval==0 and n_epi!=0:
-            print("# of episode :{}, avg score : {:.1f}".format(n_epi, score/print_interval))
-            score = 0.0
-
-        env.close()
 
 if __name__ == '__main__':
     main()
