107a264c599185c7967ba17ed0e7ffa7614537b3
Zachary_Yang <2431542525@qq.com>
2024-03-26 16:06:09 +0800

    bugfix tab

--- ppo.py
+++ ppo.py
@@ -18,7 +18,7 @@ T_horizon     = 20
 
 # swanlab init
 swanlab.init(
-    experiment_name="ppo method",
+    experiment_name="ppo_method",
     description= "a simple rl task",
     logdir="./swanlog",
 )
@@ -116,27 +116,27 @@ def main():
     
     for n_epi in range(10000):
         s, _ = env.reset()
-    done = False
-    while not done:
-        for t in range(T_horizon):
-            prob = model.pi(torch.from_numpy(s).float())
-            m = Categorical(prob)
-            a = m.sample().item()
-            s_prime, r, done, truncated, info = env.step(a)
+        done = False
+        while not done:
+            for t in range(T_horizon):
+                prob = model.pi(torch.from_numpy(s).float())
+                m = Categorical(prob)
+                a = m.sample().item()
+                s_prime, r, done, truncated, info = env.step(a)
 
-            model.put_data((s, a, r/100.0, s_prime, prob[a].item(), done))
-            s = s_prime
+                model.put_data((s, a, r/100.0, s_prime, prob[a].item(), done))
+                s = s_prime
 
-            score += r
-            if done:
-                break
+                score += r
+                if done:
+                    break
 
-        model.train_net()
+            model.train_net()
 
-    if n_epi%print_interval==0 and n_epi!=0:
-        print("# of episode :{}, avg score : {:.1f}".format(n_epi, score/print_interval))
-        swanlab.log({"avg_score": score/print_interval})
-        score = 0.0
+        if n_epi%print_interval==0 and n_epi!=0:
+            print("# of episode :{}, avg score : {:.1f}".format(n_epi, score/print_interval))
+            swanlab.log({"avg_score": score/print_interval})
+            score = 0.0
 
     env.close()
 
